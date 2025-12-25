5f411e7c2fa413b1bce8e7b921d7dc8846cb6de4
unknown <kr8940@srmist.edu.in>
2025-12-25 21:55:51 +0530

    Add test db properties and test profiles

--- src/test/java/com/shopstream/userservice/UserServiceApplicationTests.java
+++ src/test/java/com/shopstream/userservice/UserServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.shopstream.userservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UserServiceApplicationTests {
 
 	@Test
